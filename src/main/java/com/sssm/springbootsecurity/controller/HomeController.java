d9a2dc905ff03e96a48b1d5305262f631518542c
Mandar.Shidhore <mandar.learnstuff@gmail.com>
2018-10-10 21:36:50 -0600

    initial checkin

---
+++ src/main/java/com/sssm/springbootsecurity/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.sssm.springbootsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello";
+	}
+
+	@GetMapping("/getsavingsaccountbalance/{acctNumber}")
+	public int getsavingsaccountbalance(@PathVariable("acctNumber") final String acctNumb) {
+		return 100;
+	}
+
+	@GetMapping("/getcheckingaccountbalance/{acctNumber}")
+	public int getcheckingaccountbalance(@PathVariable("acctNumber") final String acctNumb) {
+		return 100;
+	}
+
+}
